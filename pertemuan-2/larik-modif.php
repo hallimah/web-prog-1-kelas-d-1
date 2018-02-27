bd6ac831f6d2752c95501683bde2215626b3f9e9
tamami <tamami.oka@gmail.comgit>
2018-02-27 16:11:15 +0700

    update materi ke-2

---
+++ pertemuan-2/larik-modif.php
@@ -0,0 +1,11 @@
+<?php
+$data = array(
+    "nama" => "tamami",
+    "usia" => 29,
+    "jomblo" => true
+);
+
+echo "NAMA : " . $data["nama"] . "<br>";
+echo "USIA : " . $data["usia"] . "<br>";
+echo "JOMBLO ? " . $data["jomblo"] . "<br>";
+?>
