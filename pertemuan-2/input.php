0ed8c321f55d9cb811051cfc0dbaabb9de502ca5
tamami <tamami.oka@gmail.comgit>
2018-02-27 16:51:50 +0700

    prosesing data

---
+++ pertemuan-2/input.php
@@ -0,0 +1,5 @@
+<form action="proses.php" method="post">
+    Nama Anda <input type="text" name="nama"><br>
+    Email <input type="text" name="email"><br>
+    <input type="submit" value="Proses">
+</form>
